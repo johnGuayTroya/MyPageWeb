af6236fa1d1e585bf6d45f1a5a9a82f50ba45687
Johnny <johnandrey@hotmail.es>
2025-03-10 22:08:30 +0100

    se suben cambios en la traduccion y se eliminan ficheros JSON

---
+++ public_html_V0.2/js/translationProjectsPages.js
@@ -0,0 +1,137 @@
+// JSON con los txt de las traducciones
+const txtJsonLang = {
+    "es": {
+            "proy": "Proyectos",
+            "AM": "Sobre mí",
+            "TROTP": "el motivo del proyecto",
+            "ITP": "Dentro del proyecto",
+            "LOI": "Links relevantes",
+            "TUITP": "Herramientas usadas",
+            "GTPR": "Ir al repositorio del proyecto",
+            "GTPF": "Ir a la memoria de desarrollo",
+            "PE": "Experiencias personales",
+            "MRITP": "Mi rol en el proyecto",
+            "ST": "Agracedimientos",
+            "DAPK":"Descargar APK",
+            "ECYMd1": "ECYM es un pequeño entrenador de mecanografía, atención y memoria en el que puedes practicar o poner a prueba tus habilidades cognitivas.",
+            "ECYMd2": "Incluye un sistema de inicio de sesión y un sistema de tabla de puntuaciones.",
+            "AM1": "Mi nombre es Johnny.",
+            "AM2": "He estudiado en ESNE, ahora se llama UDIT.",
+            "AM3": "Durante mis años de grado me centré en la parte de programación,porque creo que es una pieza clave cuando se trata de cualquier desarrollo de software,por pequeño que sea.",
+            "Sk1": "Conozco los patrones de diseño (singleton,MVC,factory,observer...),estructuras de datos,algoritmos genéticos,POO,además tengo conocimiento sólido tanto de programación como manejo del motor Unity3D(profiler,flujo de trabajo,scripting)",
+            "AM4": "A principios del 2022 Empecé como desarrollador Salesforce en Omega CRM a Merkle company y he ido adquiriendo conocimiento sobre este CRM.",
+            "Sk2": "Domino la parte front (LWC,javascript,HTML) como back (Apex,SOQL),conozco su herramienta declarativa FLOW,se manejar comunidades,crear el modelo de datos,crear reglas de validación,crear procesos de aprobación,batches...",
+            "ECYMp1": "Práctica final para la asignatura POO II curso 2018-2019(2do año)",
+            "ECYMp2": "Este poyecto surgió de la percepción que tengo acerca del aprendizaje.Pienso que los humanos aprendemos mejor cuando interactuamos con una situación ,con un problema y mediante la experimentación,analisis e imaginación llegamos a un resultado,que puede ser o no el correcto.",
+            "ECYMp3": "Inspirado por la saga de videojuegos Brain Training, decidí realizar este pequeño entrenador de mecanografía,atención y memoria ",
+            "ECYMp4": "Escogí el efecto Stroop como referencia para organizar los ejercicios :'Se llama efecto Stroop, la interferencia semántica que se produce al tratar de identificar el color en el que se escribe una palabra,cuyo significado nombra otro color'.",
+            "ECYMp5": "Este tipo de tarea presenta una dificultad en sí misma y al agregar el factor tiempo, hago de esa dificultad un desafío que el usuario tiene que superar, mientras se concentra y entrena su capacidad de atención.",
+            "ECYMp6": "Otra de mis motivaciones al desarrollar este proyecto fue perfeccionar mis habilidades de diseño y programación y aprender a usar la biblioteca MySQL en C # de tal manera que implementé un sistema de login.",
+            "ECYMp7": "El usuario a través de su 'Nick' accederá a estos ejercicios y a su vez podra ver sus puntuaciones y compararlos con otros usuarios (implementé la base de datos localmente). Para la dinámica de los juegos, extraigo de un .txt las palabras que encontramos en el juego.",
+            "SPTp1": "Práctica para la asignatura de diseño web,curso 2019-2018(2do año)",
+            "SPTp2": "En este trabajo quise poner a prueba mis conocimientos aprendidos en esta asignatura haciendo un clon del popular juego Flappy birds.",
+            "SPTp3": "Gracias a la investigación en Internet encontré el framework Phaser que incluye un sistema de física con el que pude emular la gravedad del juego. Cuando trabajé con este framework, estaba bastante familiarizado con la estructura de un script en Unity, Phaser tiene funciones create y uptade, similares a las funciones de start y update.",
+            "SPTp4": "Después de haber implementado la jugabilidad básica (salto, generación de bloques y velocidad de la nave) le di un lavado de cara creando un nuevo personaje y un fondo, dando lugar a un tema espacial. A todo esto luego le agregué sonido para darle feedback al usuario. Como complemento, implementé un formulario para que el usuario ponga su Nickname.",
+            "SDp1": "Práctica para la asignatura de animación 3D,curso 2019-2010 (3er año) Todos los modelos son hechos por mí, pero el modelo de la carretera y la textura no y modifiqué un script de Internet para hacer temblar la cámara",
+            "SDp2": "En esta práctica quería poner a prueba mis habilidades de modelado, texturizado y la recién aprendida de animación",
+            "SDp3": "En el enlace de abajo encontrarás el diario de desarrollo de este corto, el archivo es un PDF llamado'Johnny_Guayanay_Memoria 'donde explico las referencias que he seguido, y mi metodología a la hora de realizar este proyecto.",
+            "DGp1": "El evento comenzó el 31 de Enero a las 10:00 am y terminó el dos de febrero a la una de la tarde",
+            "DGp2": "Este pequeño juego es el resultado de mi primer contacto con las game jams, recuerdo estar nervioso pero al mismo tiempo quería saber qué iban a ser capaces de hacer cinco extraños. Me vino una sensación de vacío Cuando terminamos el evento, porque estuvimos trabajando el fin de semana creando al Dr. Gendro y su pequeño laboratorio, esa descarga de adrenalina que te invade por ayudar a terminar el proyecto había desaparecido y no tuve más remedio que lidiar con el 'efecto rebote'. ",
+            "DGp3": "El resultado final resultó ser de lo más variopinto ya que teníamos a Caye con un estilo naif y Luis uno lúgubre, esta mezcla de influencias en mi opinión le da mucha personalidad al proyecto.Esto unido a las ganas que tenía nuestro inexperto rigger 2D John, logramos fusionar todo nuestro conocimiento y estilo en este peculiar trabajo.",
+            "DGp4": "El papel que desempeñé fue el de diseñador pero sin embargo ayudé a nuestro programador Adrián Botana Pérez con el código de los menús, también me ocupé de las animaciones del menú principal, pulí los frascos añadiendoles unas burbujas y jugué con un efecto de luz",
+            "DGp5": "Este evento se pudo realizar gracias a dos profesores de Esne Asturias: Jonathan Rivas y Ulises Martinez Piña (trajo Juegos como premio), mi agradecimiento a estos profesores",
+            "TRp1": "Tempus runit es un juego para dispositivos móviles que mezcla el género endless run con un cuestionario en el que si chocas con un obstáculo y aciertas la pregunta, continúas jugando. Ayuda a Mark a conseguir los potenciadores que le ayudarán a pasar a los siguientes niveles. Este juego incorpora un sistema de High score para que intentes obtener los mejores resultados cada vez que inicies un nuevo juego",
+            "TRp2":"EL proyecto se hizo con la ayuda de otros dos miembros(Adrián Soto-Artísta y Daniel Guerra-programador).",
+            "TRp3": "Me encargé de la programación del cuestionario (las preguntas y respuestas están en un archivo .txt), así que hice un script que lee el .txt y devuelve todas las respuestas y preguntas, para luego poner las palabras en los botones y por pantlla.",
+            "TRp4": "También hice un script para el flujo de los menús,la creación de obstáculos, PU y monedas procedurales.También hice los scripts de los power ups, el sistema de guardar y cargar estadísticas como monedas obtenidas y High score y ayudé con la parte de animación ( monedas y PU).",
+            "MTp1": "Music Translator es una herramienta de traducción de canciones en la cual podemos seleccionar nuesta canción favorita,escibir la traducción en el lenguaje que queramos,podremos guardar el progreso en un formato similar al RST,también incluye la funcionalidad de serialización.",
+            "MTp2": "Para este proyecto nos decantamos por el sistema modelo controlador y vista,Hemos utilizado el lenguaje c++ para construir el BackEnd y Unity como FrontEnd.Gracias a la creación de la Dll comunicamos las clases y métodos que usamos dentro de la herramienta.",
+            "MTp3": "En el enlace de abajo encontrarás el manual de uso de este proyecto."
+        },
+    "en": {
+        "proy": "Projects",
+        "AM": "About me",
+        "TROTP": "the reason of the project",
+        "ITP": "Inside  the  project",
+        "LOI": "Links of interest",
+        "TUITP": "Tools used in this project",
+        "GTPR": "Go to project repository",
+        "GTPF": "Go to development diary",
+        "PE": "Personal Experiences",
+        "MRITP": "My rol in the project",
+        "ST": "Special Thanks",
+        "DAPK":"Dowload APK",
+        "AM1": "My name is Johnny.",
+        "AM2": "I studied in ESNE,now it called UDIT",
+        "AM3": "During my undergraduate years I focused on the programming part, because I believe that it is a key piece when it comes to any software development, no matter how small.",
+        "Sk1": "I have knowledge about design patterns (singleton, MVC, factory, observer...),data structures, genetic algorithms, OOP,web design and I know both programming and working in the Unity3D program (workflow, profiler, scripting...).",
+        "AM4": "In 2022 earlies I started as Salesforce developer in Omega CRM a Merkle company and I have adquired experience about this CRM.",
+        "Sk2": "I have strong knowledge about front-end (LWC,javascript,HTML) as back-end (Apex, SOQL), I know your declarative tool FLOW, I know how to manage communities, create the data model, create validation rules, create approval processes, batches...",
+        "ECYMp1": "Final work for the subject OOP II Academy year 2018-2019 (2nd year)",
+        "ECYMp2": "This project arose from the perception that I have about learning.I thin that humans learn best when we interact with a situation with a problem and through experimentation,analysis and imagination we arrive at a result, which may or may not be the correct one.",
+        "ECYMp3": "Also inspired by the Brain Training videogame saga,I decided to do this littel typing,attention and memory trainer",
+        "ECYMp4": "I took the Stroop effect from references to perform one of the exercises'It is called the Stroop effect, the semantic interference that occurs when trying to identify the color in which a word is written, whose meaning names another color'.",
+        "ECYMp5": "This type of task presents a difficulty in itself and by adding the time factor,I make that difficulty a challenge that the user has to overcome, while he concentrates and trains his attention span.",
+        "ECYMp6": "Another of my motivations in developing this project used to upgrade my design and programming skills and learn how to use the MySQL library in C#,for this reason I implemented a login system.",
+        "ECYMp7": "The user through his 'Nick' will access these exercises and then he will be able to see his scores and compare them with other users (I implemented the database locally). For the gameplay, I geted from a .txt the words that we find in the game.",
+        "SPTp1": "Work for the subject web design Academy year 2018-2019 (2nd year)",
+        "SPTp2": "In this work I wanted to test my knowledge learned in this subject by making a clone of the popular game Flappy birds.",
+        "SPTp3": "Thanks to internet research I found the Phaser framework that includes a system of physics with which I was able to emulate the gravity of the game. When I working with this framework, I was quite familiar with the structure of a basic script in Unity,Phaser has a create and update functions, similar to Start and update functions.",
+        "SPTp4": "After having implemented the basic gameplay (jump, block generation and ship speed)I gave it a facelift by creating a new character and a background,giving rise to a space theme.To all this I then added sound to give feedback to the user.As an addition I implemented a form for the user to put his Nickname.",
+        "SDp1": "Work for the subject 3D animation Academy year 2019-2010 (3nd year) All models are made by me,but road model and texture not and I modified a internet script to make the camera shake.",
+        "SDp2": "In this practice I wanted to put my modeling, texturing and newly learned animation skills to the test.",
+        "SDp3": "In the link below you will find the development diary of this short,the file is a PDF called 'Johnny_Guayanay_Memoria'where I explain the references that I have followed,and my methodology when carrying out this project.",
+        "DGp1": "The event started on January 31 at 10:00 am and ended on February 2 at 1:00 pm",
+        "DGp2": "This little game is the result of my first contact with game jams, I remember being nervous  but at the same time I wanted to know what five strangers were going to be able to do. A feeling of emptiness came to me when we finished the event,because we were working the weekend creating Dr. Gendro and his little laboratory, that adrenaline rush that invades you for helping to finish the project had disappeared a and I had no choice to deal with the 'rebound effect'.",
+        "DGp3": "The final result turned out to be the most varied as we had Caye with a 'cute' (naif) style and Luis a gloomy one, this mixture of styles in my opinion gives the project a lot of personality.All this together with the desire that our inexperienced rigger 2D John had,we managed to merge all our knowledge and style in this peculiar work.",
+        "DGp4": "The role I played was that of designer but nevertheless I helped our programmer Adrián Botana Pérez with the code of the menus, I also took care of the main menu animations, polished the jars with bubble effect and played with a light effect.",
+        "DGp5": "This event was possible thanks to two teachers from Esne Asturias: Jonathan Rivas and Ulises Martinez Piña (he brought Games as a prize), my thanks to these teachers.",
+        "TRp1": "Tempus runit is a game for mobile devices that mixes endless runner gameplay with a quiz where if you hit an obstacle and you answer the question, you continue playing.Help Mark to get the power ups that will help him to pass to the next levels.  This game incorporates a high score system for you to try to get the best results every time you start a new game.",
+        "TRp2":" The project was made by other two members(Adrián Soto-art and Daniel Guerra-programmer).",
+        "TRp3":" I have done the programming of the quiz(the questions and answers are be in a .txt file),so I made a script that read the .txt and return all answers and questions,for later put the words in a buttons.",
+        "TRp4":"I also made a script for the flow of the menus, the creation of obstacles, PU and procedural coins. I also made the scripts of the power ups, the system of saving and loading statistics such as coins obtained and High score and I helped with the animation part (coins and PU).",
+        "MTp1": "Music Translator is a song translation tool in which we can select our favorite song, write the translation in the language we want, we can save the progress in a format similar to RST, it also includes serialization functionality.",
+        "MTp2": "For this project we opted for the controller and view model system, We have used the c ++ language to build the BackEnd and Unity as FrontEnd.Thanks to the creation of the Dll we communicate the classes and methods that we use within the tool.",
+        "MTp3": "In the link below you will find the manual for using this project."
+    }
+};
+
+function languageSwapController() {
+    // Establece el idioma inicial si no está configurado
+    if (!localStorage.getItem("lang")) {
+        localStorage.setItem("lang", "en");
+    }
+    let lang = localStorage.getItem("lang");
+
+    // Función para traducir los elementos
+    const translate = () => {
+        $('.lang').each(function () {
+            $(this).text(txtJsonLang[lang][$(this).attr('key')]);
+        });
+    };
+
+    // Traduce al cargar la página
+    translate();
+
+    // Cambia el idioma al hacer clic en los botones
+    $('.translate').click(function () {
+        lang = $(this).attr('id'); 
+        localStorage.setItem("lang", lang);
+        translate();
+    });
+
+    // Cambia el color de los botones al hacer clic
+    $("#es").click(function () {
+        $(this).css("color", "#F1CA06");
+        $("#en").css("color", "white");
+    });
+    $("#en").click(function () {
+        $(this).css("color", "#F1CA06");
+        $("#es").css("color", "white");
+    });
+}
+
+
+$(document).ready(function(){
+	languageSwapController();;
+});
